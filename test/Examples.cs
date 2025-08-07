6537c2dc40340155a335d29796358a8c0aa99ace
Hossein Montazeri <h.montaz.ca@gmail.com>
2025-08-06 20:10:10 -0700

    test 1

--- test/Examples.cs
+++ test/Examples.cs
@@ -11,7 +11,13 @@ namespace hmlib.PersianDateTests
 {
 	public class Examples
 	{
-		/*[Fact]
+
+		public Examples()
+		{
+			CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+		}
+
+		[Fact]
 		public void ToStringExamples()
 		{
 			JalaliDateTime j = new DateTime(2024, 01, 01);
@@ -21,10 +27,10 @@ namespace hmlib.PersianDateTests
 			var s3 = j.ToString(CultureInfo.GetCultureInfo("fa-IR"));// "۱۴۰۲/۱۰/۱۱ ۱۲:۰۰:۰۰ ق.ظ"
 			var s4 = j.ToString("dddd dd MMMM yyyy", CultureInfo.GetCultureInfo("fa-IR"));// "دوشنبه ۱۱ دی ۱۴۰۲"
 
-			Assert.Equal("1402/10/11 12:00:00 AM", s1);
+			Assert.Equal("1402/10/11 00:00:00", s1);
 			Assert.Equal("1402/10/11 12:00:00 AM", s2);
 			Assert.Equal("۱۴۰۲/۱۰/۱۱ ۱۲:۰۰:۰۰ ق.ظ", s3);
 			Assert.Equal("دوشنبه ۱۱ دی ۱۴۰۲", s4);
-		}*/
+		}
 	}
 }
