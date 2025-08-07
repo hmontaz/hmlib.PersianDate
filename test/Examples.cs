42b72fef300753811a539bdf5827d1b8ae28dbcb
Hossein Montazeri <h.montaz.ca@gmail.com>
2025-08-06 21:52:21 -0700

    test 3

--- test/Examples.cs
+++ test/Examples.cs
@@ -14,7 +14,7 @@ namespace hmlib.PersianDateTests
 
 		public Examples()
 		{
-			CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+			CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 		}
 
 		[Fact]
@@ -27,7 +27,7 @@ namespace hmlib.PersianDateTests
 			var s3 = j.ToString(CultureInfo.GetCultureInfo("fa-IR"));// "۱۴۰۲/۱۰/۱۱ ۱۲:۰۰:۰۰ ق.ظ"
 			var s4 = j.ToString("dddd dd MMMM yyyy", CultureInfo.GetCultureInfo("fa-IR"));// "دوشنبه ۱۱ دی ۱۴۰۲"
 
-			Assert.Equal("1402/10/11 00:00:00", s1);
+			Assert.Equal("1402/10/11 12:00:00 AM", s1);
 			Assert.Equal("1402/10/11 12:00:00 AM", s2);
 			Assert.Equal("۱۴۰۲/۱۰/۱۱ ۱۲:۰۰:۰۰ ق.ظ", s3);
 			Assert.Equal("دوشنبه ۱۱ دی ۱۴۰۲", s4);
