03d627b9d518f2ec135540b91f56727705629a78
Hossein Montazeri <h.montaz.ca@gmail.com>
2025-08-06 19:17:46 -0700

    add missing file

---
+++ test/JalaliDateTimeTests/JalaliDateTimeParseTests.cs
@@ -0,0 +1,24 @@
+﻿using hmlib.PersianDate.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace hmlib.PersianDateTests.JalaliDateTimeTests
+{
+	public class JalaliDateTimeParseTests
+	{
+		[Fact]
+		public void TokenizeFormatTest()
+		{
+			var parser = new JalaliDateTimeParser();
+			var format = "yyyy/MM/dd HH:mm:ss";
+			var tokens = parser.TokenizeFormat(format);
+			var expectedTokens = new List<string> { "yyyy", "/", "MM", "/", "dd", " ", "HH", ":", "mm", ":", "ss" };
+
+			Assert.Equal(expectedTokens, tokens);
+		}
+	}
+}
